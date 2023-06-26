0f8eb9fcb657be9063ef92241354264d2e75d0f3
KimGeantae <128386707+KimGeantae@users.noreply.github.com>
2023-06-26 16:13:48 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -146,58 +146,50 @@ int main(){
 
             else if(mode ==1) {//원을 돌고 난 뒤 상태 (두 번째 안) 카메라 사용
                 if(ir_plusval[0]==false){
-                    if(data[1]<60){//가깝지 않을 때 원타기
+                    if(data[1]<dis && ir_val[6]<black){//가깝지 않을 때 원타기
                         speedL = 0.3;
                         speedR = 0.8;
                     }
-                    if(ang >= 70 && ang <= 110 & ir_val[0]>black && ir_val[1]>black){//적이 정면에 있을 때 직진
+                    if(ang >= 70 && ang <= 110){//적이 정면에 있을 때 직진
                         speedL = 1;
                         speedR = 1;
                     }
-                    else if(ir_plusval[1]==false || ir_plusval[2]==false ||ir_plusval[3]==false){
-                        DC_follow();
+                    else if(ir_plusval[7] == true){
+                        if((ang<70 || ang>110) && psdfl_val < 30){
+                            //적을 쫓아가서 직진하다가 적이 옆으로 빠지고 
+                            //우리가 다시 트래킹하기 전에 벽에 너무 가까운 상태여서 박히는 경우
+                            mode = 21;
+                        }
+                        else if (data[0] == 999 && psdfl_val < 30){//카메라,  서보 오류로 적을 잃어버리거나 찾지 못해서 벽에 박히는 경우
+                            mode = 22;
+                        }
+                        else{
+                            DC_follow();
+                        }
+
                     }
+                    
                 }
 
                 else if(ir_plusval[0]==true){//앞쪽 색영역 봤을 때
-                        if(psdfl_val >80 && psdfl_val <30){
-                            if(data[1]>dis && ir_val[5]<black && ir_val[6]<black /*&& ir_val[8]>black && ir_val[9]>black/*/){
-                            speedL = 0;
-                            speedR = 0;
-                            }
-                            else if(data[1]>dis && ir_val[5]>black && ir_val[6]>black){
-                                speedL = 1;
-                                speedR = 1;
-                            }
-                            else if(data[1]<dis){
-                                speedL = 0.6;
-                                speedR = -0.8;
                         
-                            }
-                            else if(ir_plusval[6]==true && ir_val[2]<black){
-                                speedL = 0.0;
-                                speedR = 0.0; 
-                            }
-                        }
-                        else if(psdfl_val <80 && psdfl_val >30){
-                                if(data[1]>dis && ir_val[5]<black && ir_val[6]<black /*&& ir_val[8]>black && ir_val[9]>black/*/){
-                                speedL = 0;
-                                speedR = 0;
-                                }
-                                else if(data[1]>dis && ir_val[5]>black && ir_val[6]>black){
-                                    speedL = 1;
-                                    speedR = 1;
-                                }
-                                else if(data[1]<dis){
-                                    speedL = 0.6;
-                                    speedR = -0.8;
+                    if(data[1]>dis && ir_val[5]<black && ir_val[6]<black /*&& ir_val[8]>black && ir_val[9]>black/*/){
+                        speedL = 0;
+                        speedR = 0;
+                    }
+                    else if(data[1]>dis && ir_val[5]>black && ir_val[6]>black){
+                        speedL = 1;
+                        speedR = 1;
+                    }
+                    else if(data[1]<dis){
+                        speedL = 0.6;
+                        speedR = -0.8;
+                    }
+                    else if(ir_plusval[6]==true && ir_val[2]<black){
+                            speedL = 0.0;
+                            speedR = 0.0; 
+                    }
                         
-                                }
-                                else if(ir_plusval[6]==true && ir_val[2]<black){
-                                    speedL = 0.0;
-                                    speedR = 0.0; 
-                                }
-                        }
                         //mode=2;
                 }
                 else if(ir_plusval[7]==true){
@@ -223,6 +215,41 @@ int main(){
                     mode = 1;
                 }
             }
+            else if (mode == 21){
+                //적을 쫓아가서 직진하다가 적이 옆으로 빠지고 
+                //우리가 다시 트래킹하기 전에 벽에 너무 가까운 상태여서 박히는 경우
+                //후진을 해야하는지 실험적으로 해봐야 함
+                while(ang<70 || ang>110){
+                    if(ang>110){//마지막으로 적을 우측에서 봤을 경우
+                       speedL= 0.2;
+                       speedR= -0.2;
+                    }
+                   if(ang<70){//마지막으로 적을 좌측, 중앙에서 봤을 경우 / preread값이 없는 경우
+                       speedL= -0.2;
+                       speedR= 0.2;
+                    }
+                }
+                speedL=0;
+                speedR=0;
+                mode=1;
+            }
+
+            else if(mode ==22){//카메라,  서보 오류로 적을 잃어버리거나 찾지 못해서 벽에 박히는 경우
+                while (data[0] ==999){
+                   if(preread == 'R'){//마지막으로 적을 우측에서 봤을 경우
+                       speedL= 0.2;
+                       speedR= -0.2;
+                    }
+                   else{//마지막으로 적을 좌측, 중앙에서 봤을 경우 / preread값이 없는 경우
+                       speedL= -0.2;
+                       speedR= 0.2;
+                    }
+                }
+                speedL=0;
+                speedR=0;
+                mode=1;
+            }
+
 
             // else if(mode ==20){//벽에 부딪힐거같은 경우
 
